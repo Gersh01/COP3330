c4fca1d6c3d26d7817abe8c692fc594d8f3acb1b
User1 <alex.gershfeld@gmail.com>
2023-09-21 19:26:52 -0230

    update P2

--- COP3502/P2/movieline.c
+++ COP3502/P2/movieline.c
@@ -17,26 +17,45 @@ typedef struct customerNode {
     customerNode* node;
 } customerNode;
 
-typedef struct queue {
+typedef struct q {
     customerNode* front;
     customerNode* back;
     int size;
-} queue;
+} q;
 
 //Adds the newly made customer into a linked list
 customerNode* createNode();
 //Creates a new customer, populating the struct with input values
-customerInfo* createCustomer();
+customerInfo* createCustomer(char* newName, int newTickets, int newArrTime);
 
 int main() {
+    int custAmt, booths;
+    customerInfo* tmpCustomer;
+    char* tmpName;
+    int tmpTickets, tmpLineNum, tmpArrTime;
+    printf("Enter the amount of customers and then the amount of booths\n");
+    scanf("%d %d", &custAmt, &booths);
+
+    printf("Please enter the customers coming into line\n");
+    while(custAmt != 0) {
+        scanf("%s %d %d", tmpName, &tmpTickets, &tmpArrTime);
+        tmpCustomer = createCustomer(tmpName, tmpTickets, tmpArrTime);
+
+        custAmt--;
+    }
 
     return 0;
 }
 
+customerInfo* createCustomer(char* newName, int newTickets, int newArrTime) {
+    
+
+}
 customerNode* createNode() {
 
 }
-customerInfo* createCustomer() {
+
+q* createQ() {
 
 }
 
