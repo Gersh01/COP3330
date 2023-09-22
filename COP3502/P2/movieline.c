3e851925858e081e6f3dfa4e0ce66608908ddf8d
Alex Gershfeld <134902494+Gersh01@users.noreply.github.com>
2023-09-21 23:52:04 -0400

    Update movieline.c

--- COP3502/P2/movieline.c
+++ COP3502/P2/movieline.c
@@ -14,7 +14,7 @@ typedef struct customerInfo {
 
 typedef struct customerNode {
     customerInfo* customer;
-    customerNode* node;
+    struct customerNode* nextNode;
 } customerNode;
 
 typedef struct q {
@@ -24,40 +24,104 @@ typedef struct q {
 } q;
 
 //Adds the newly made customer into a linked list
-customerNode* createNode();
+customerNode* createNode(customerInfo* newCustomer);
+//Inserts new node to the front of the list
+customerNode* insertToFront(customerNode* newNode, customerInfo* newCustomer);
 //Creates a new customer, populating the struct with input values
 customerInfo* createCustomer(char* newName, int newTickets, int newArrTime);
+//Calculates the queue number for each customer
+int queueNum(q** queues, customerInfo* newCustomer);
+//Creates the memory for the queue
+q* createQ(int lineNo);
 
 int main() {
-    int custAmt, booths;
-    customerInfo* tmpCustomer;
-    char* tmpName;
-    int tmpTickets, tmpLineNum, tmpArrTime;
-    printf("Enter the amount of customers and then the amount of booths\n");
-    scanf("%d %d", &custAmt, &booths);
+    
+    customerInfo* tmpCustomer = NULL;
+    customerNode* customerList = NULL;
+    q** allQueues = (q**)calloc(QUEUESIZE, sizeof(q*));
 
-    printf("Please enter the customers coming into line\n");
+    char tmpName[MAXCHAR];
+    int custAmt, booths;
+    int tmpTickets, tmpLineNum, tmpArrTime;
+
+    scanf("%d %d", &custAmt, &booths); //input the amount of customers and functioning booths
+    
+    //Itemizes all of the input customers into one concise linked list; to then be processed one
+    //list item at a time. 
     while(custAmt != 0) {
         scanf("%s %d %d", tmpName, &tmpTickets, &tmpArrTime);
+
+        //Create the customer struct and populate, making sure no memory errors occur.
         tmpCustomer = createCustomer(tmpName, tmpTickets, tmpArrTime);
+        if(tmpCustomer == NULL) {
+            printf("Error allocating customer\n");
+            return 0;
+        }
+
+        //Calculates the queue number based on the first character of their name
+        tmpCustomer->lineNum = queueNum(allQueues, tmpCustomer);
+
+        
+        customerList = insertToFront(customerList, tmpCustomer);
 
         custAmt--;
     }
+    
 
+
+    free(tmpCustomer->name);
+    free(tmpCustomer);
     return 0;
 }
 
 customerInfo* createCustomer(char* newName, int newTickets, int newArrTime) {
+    customerInfo* tmp = (customerInfo*)malloc(sizeof(customerInfo));
     
+    //allocate and assign name for customer
+    tmp->name = (char*)calloc(MAXCHAR, sizeof(char));
+    if(tmp->name != NULL) 
+        strcpy(tmp->name, newName);
+    else return 0;
+
+    //assign number of tickets and arrival time
+    tmp->numOfTickets = newTickets;
+    tmp->arrivalTime = newArrTime;
+
+    return tmp;
+}
+customerNode* createNode(customerInfo* newCustomer) {
+    customerNode* tmp = (customerNode*)malloc(sizeof(customerNode));
+
+    tmp->customer = newCustomer; 
+    tmp->nextNode = NULL;
+
+    return tmp;
+}
+customerNode* insertToFront(customerNode* newNode, customerInfo* newCustomer) {
+    customerNode* tmpNode = createNode(newCustomer);
+    tmpNode->nextNode = newNode;
+
+    return tmpNode;
+}
+int queueNum(q** queues, customerInfo* newCustomer) {
+    //calculate the assigned queue
+    int ret = 0;
+    int position = (newCustomer->name[0] - 'A') % 13; 
+    if(queues == NULL)
+        return ret;
+    else if(position != 0)
+        ret = position;
+    
+    //rule for when mod is zero -- possibly move into its own function
+    else if(position == 0) {
+        for(int i = 0; i < QUEUESIZE; i++) 
+            if(queues[i]->size != 0)
+                for(int j = 1; j < QUEUESIZE; j++) 
+                    if(queues[i]->size > queues[j]->size) ret = j;
+    }
+
+    return ret;
+}
+q* createQ(int lineNo) {
 
 }
-customerNode* createNode() {
-
-}
-
-q* createQ() {
-
-}
-
-
-
