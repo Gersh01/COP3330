b8a31021a3ffdad2baa3542f14656213c972f3a4
Alex Gershfeld <134902494+Gersh01@users.noreply.github.com>
2023-09-23 21:21:17 -0400

    Update DriverClass.java

--- COP3330/DriverClass.java
+++ COP3330/DriverClass.java
@@ -1,17 +1,21 @@
-import java.util.Scanner;
+//
+
 import java.util.ArrayList;
 
 public class DriverClass {
     public static void main(String[] args) {
         String fullName = "Erika T. Jones";
         String employeeNumber = "ej789";
-        double payRate = 100.0, hoursWorked = 1.0;
+        double payRate = 10.50, hoursWorked = 36.00;
 
         // TA will change the pay rate and the hours worked to test your code
         Employee e;
         e = new Employee(fullName, employeeNumber, payRate, hoursWorked);
         System.out.println(e); // To Test your toString method
+
+
         e.printCheck(); // This prints the check of Erika T. Jones
+        /*
         Company company = new Company();
         company.hire ( new Employee ("Saeed Happy", "sh895" , 2 , 200) );
         company.hire (e);
@@ -27,6 +31,7 @@ public class DriverClass {
         System.out.println( company.SearchByName("WaLiD WiLLiAms") );
         company.printEmployees();
         System.out.println("Bye!");
+        */
     }
 }
 //____________________________
@@ -62,16 +67,25 @@ class Employee {
     public String toString() {
         return "[" + employeeNumber + "/" + fullName + ", " + hoursWorked + " Hours @ " + payRate + " per hour]";
     }
-
+    private double grossPay() {
+        return (payRate * hoursWorked);
+    }
     private double netPay() {
-
-        return 0;
+        return grossPay() * .94;
     }
 
     public void printCheck() {
-
+        System.out.println("---------------------------------------------------------------------------");
+        System.out.println("\t\tEmployee's name:\t\t" + getFullName());
+        System.out.println("\t\tEmployee's number:\t\t" + getEmployeeNumber());
+        System.out.println("\t\tHourly rate of pay:\t\t" + getPayRate());
+        System.out.println("\t\tHours worked:\t\t\t" + getHoursWorked() + "\n");
+        System.out.printf("\t\tTotal Gross Pay:%14.2f\n\n", grossPay());
+        System.out.println("\t\tDeductions");
+        System.out.printf("\t\tTax (6 %%):%19.2f\n\n", grossPay() - netPay());
+        System.out.printf("\t\tNet Pay:%22.2f\n\n", netPay());
+        System.out.println("---------------------------------------------------------------------------");
     }
-
 }
 //____________________________
 class Company {
@@ -89,42 +103,41 @@ class Company {
         companyTaxId = "v1rtua7C0mpan1";
     }
     public boolean hire ( Employee employee ) {
-    //Add employee to employeeList
-    //Note well that we can't add an employee whose employeeNumber already
-    //assigned to another employee. In that case, this method returns false.
-    //This method returns true otherwise
+        //Add employee to employeeList
+        //Note well that we can't add an employee whose employeeNumber already
+        //assigned to another employee. In that case, this method returns false.
+        //This method returns true otherwise
         return true;
     }
     public void printCompanyInfo() {
-    //This method prints the company name, the tax id and the current number of employees
-    //You may choose to print that any way you like!
+        //This method prints the company name, the tax id and the current number of employees
+        //You may choose to print that any way you like!
     }
     public void printEmployees() {
-    //This method prints all employees (One employee per line)
-    //Note that you already have toString in Employee
+        //This method prints all employees (One employee per line)
+        //Note that you already have toString in Employee
     }
     public int countEmployees( double maxSalary ) {
-    //This method returns the number of employees paid less than maxSalary
+        //This method returns the number of employees paid less than maxSalary
         return 0;
     }
     public boolean SearchByName (String fullName ) {
-    //This method returns true if fullName exists as an employee.
-    //It returns false otherwise
-    //this is a not a case-sensitive search.
+        //This method returns true if fullName exists as an employee.
+        //It returns false otherwise
+        //this is a not a case-sensitive search.
         return false;
     }
     public void reverseEmployees () {
-    //This method reverses the order in which the employees were added to
-    //the list. The last employee is swapped with the first employee, the
-    //second last with the second and so on
+        //This method reverses the order in which the employees were added to
+        //the list. The last employee is swapped with the first employee, the
+        //second last with the second and so on
     }
     public void deleteEmployeesBySalary (double targetSalary ) {
-    //This method deletes all employees who are paid targetSalary as a gross //salary
+        //This method deletes all employees who are paid targetSalary as a gross //salary
     }
     public void printCheck ( String employeeNumber) {
-    //This method prints the check of the employee whose employee number is
-    //employeeNumber. It prints NO SUCH EMPLOYEE EXISTS if employeeNumber is
-    //not a registered employee number.
+        //This method prints the check of the employee whose employee number is
+        //employeeNumber. It prints NO SUCH EMPLOYEE EXISTS if employeeNumber is
+        //not a registered employee number.
     }
 }
-
