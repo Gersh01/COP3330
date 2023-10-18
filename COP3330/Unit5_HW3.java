47bd3843ab7ffecbe8ae6c24cda2a8c4abb9ef95
Alex Gershfeld <134902494+Gersh01@users.noreply.github.com>
2023-10-17 22:45:09 -0400

    Update Unit5_HW3.java

--- COP3330/Unit5_HW3.java
+++ COP3330/Unit5_HW3.java
@@ -1,9 +1,10 @@
 //Unit5_HW3
-//Alexander Gershfeld, Tyler Huynh, Gabriel Perez
+//Alexander Gershfeld and Gabriel Perez
 //Hatim Boustique
 //COP3330
 //October 15, 2023
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Driver {
@@ -12,10 +13,8 @@ public class Driver {
 
         Scanner input = new Scanner(System.in);
 
-        String bookEntry, bookType, author, title, isbnStr; //information that all books have
-
+        String bookEntry, bookType, author = null, title = null, isbnStr = null; //information that all books have
         String [] bookInfo;
-        int isbn; //pass this number instead of the string version
 
         System.out.println("Welcome to the book program!");
 
@@ -35,33 +34,19 @@ public class Driver {
                 System.out.println("Please enter the author, title and the isbn of the book separated by /: ");
 
                 //Gets the information for author title and isbn ---------------------------------
-                try {
-                    bookEntry = input.nextLine();
-                    bookInfo = bookEntry.split("/");
-                    author = bookInfo[0];
-                    title = bookInfo[1];
-                    isbnStr = bookInfo[2];
-                }
-                catch(ArrayIndexOutOfBoundsException e) {
-                    System.out.println("You did not provide a valid selection, make sure there's an author/title/isbn" +
-                            ". Please try again: ");
 
-                    bookEntry = input.nextLine();
-                    bookInfo = bookEntry.split("/");
-                    author = bookInfo[0];
-                    title = bookInfo[1];
-                    isbnStr = bookInfo[2];
+                bookEntry = input.nextLine();
+                bookInfo = bookEntry.split("/");
+
+                author = bookInfo[0];
+                title = bookInfo[1];
+                isbnStr = bookInfo[2];
+
+                if(Blist.getBook(isbnStr) != null) {
+                    System.out.println("Sorry this ISBN is already taken");
+                    continue;
                 }
 
-                try {
-                    isbn = Integer.parseInt(isbnStr);
-                }
-                catch(NumberFormatException e) {
-                    System.out.println("ISBN is not a valid number, please insert the correct ISBN only: ");
-
-                    isbnStr = input.nextLine();
-                    isbn = Integer.parseInt(isbnStr);
-                }
                 //--------------------------------------------------------------------------------
 
                 System.out.println("Got it!");
@@ -81,7 +66,7 @@ public class Driver {
 
                 //Takes in more information for bookstore books-----------------------------------
                 if(bookType.compareToIgnoreCase("bb") == 0) {
-                    int percent; //pass this through
+                    double percent; //pass this through
                     double price; //pass this through
 
                     String sale, percentage;
@@ -107,36 +92,38 @@ public class Driver {
                         sale = input.nextLine();
                     }
 
-                    System.out.println("Deduction percentage: ");
-                    percentage = input.nextLine();
+                    if(sale.compareToIgnoreCase("n") == 0) percent = 0;
 
-                    try {
-                        parsePercent = percentage.split("%");
-                        percent = Integer.parseInt(parsePercent[0]);
-                    }
-                    catch(NumberFormatException e) {
-                        System.out.println("That is not a number, try again: ");
+                    else {
+                        System.out.println("Deduction percentage: ");
                         percentage = input.nextLine();
-                        parsePercent = percentage.split("%");
-                        percent = Integer.parseInt(parsePercent[0]);
+
+                        try {
+                            parsePercent = percentage.split("%");
+                            percent = Integer.parseInt(parsePercent[0]);
+                        }
+                        catch (NumberFormatException e) {
+                            System.out.println("That is not a number, try again: ");
+                            percentage = input.nextLine();
+                            parsePercent = percentage.split("%");
+                            percent = Integer.parseInt(parsePercent[0]);
+                        }
                     }
 
                     System.out.println("Got it!");
 
-                    //pass all of the book information through the book class, then into BookstoreBook
-                    Blist.addBook(new //Constructor goes here);
-                    System.out.println("\n\nHere is your bookstore book information");
-                    //method that calls the toString of the book
+                    Blist.addBook(new BookstoreBook(author, title, isbnStr, percent, sale, price));
+                    System.out.println("\nHere is your bookstore book information");
+                    System.out.println(Blist.getBook(isbnStr));
                 }
                 //--------------------------------------------------------------------------------
 
                 //Now does the same but for library books; there are less modifiers for this type--
                 else if(bookType.compareToIgnoreCase("lb") == 0) {
 
-                    //pass all of the book information through the book class, then into LibraryBook
-                    Blist.addBook(new //Constructor goes here);
-                    System.out.println("\n\nHere is your library book information");
-                    //method that calls the toString of the book
+                    Blist.addBook(new LibraryBook(author, title, isbnStr));
+                    System.out.println("\nHere is your library book information");
+                    System.out.println(Blist.getBook(isbnStr));
                 }
                 //--------------------------------------------------------------------------------
 
@@ -156,16 +143,207 @@ public class Driver {
 }
 //_________________________________
 abstract class Book {
-    //code of the abstract class Book
-    //You may add an abstract method if you see fit...
+    private String author;
+    private String title;
+    private String isbn;
+    
+    //get and set author
+    public String getAuthor() {
+        return author.toUpperCase();
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    
+    //get and set title
+    public String getTitle() {
+        return title.toUpperCase();
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    //get and set isbn
+    public String getIsbn() {
+        return isbn;
+    }
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    //constructors
+    public Book() {
+        author = "NO AUTHOR FOUND";
+        title = "TITLE NOT FOUND";
+        isbn = "ISBN NOT FOUND";
+    }
+    public Book(String isbn) {
+        this.author = "NO AUTHOR FOUND";
+        this.title = "TITLE NOT FOUND";
+        this.isbn = isbn;
+    }
+    public Book(String title, String isbn) {
+        this.author = "NO AUTHOR FOUND";
+        this.title = title;
+        this.isbn = isbn;
+    }
+    public Book (String author, String title, String isbn) {
+        this.author = author;
+        this.title = title;
+        this.isbn = isbn;
+    }
+    
+    //to String but is overridden
+    public String toString() {
+        return "[" + "-" + getIsbn() + getTitle() + " by " + getAuthor() + "]";
+    }
 }
 //_________________________________
 class BookstoreBook extends Book {
-    //fields and specific code to the BookstoreBook class goes here
+    private double discount;
+    private String onSale;
+    private double listPrice;
+
+    //get and set discount
+    public double getDiscount() {
+        if(getOnSale().compareToIgnoreCase("n") == 0) {
+            return discount;
+        }
+
+        return discount/100;
+    }
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    //get and set sale flag
+    public String getOnSale() {
+        return onSale;
+    }
+    public void setOnSale(String onSale) {
+        this.onSale = onSale;
+    }
+
+    //get and set price
+    public double getPrice() {
+        return listPrice;
+    }
+    public void setPrice(double listPrice) {
+        this.listPrice = listPrice;
+    }
+
+    //get the price after discount
+    public double getPriceReduction() {
+        return getPrice()-(getPrice()*getDiscount());
+    }
+
+    //constructors
+    public BookstoreBook () {
+        discount = 0.0;
+        listPrice = 0.0;
+        onSale = "SALE NOT FOUND";
+    }
+    public BookstoreBook (String isbn, double discount, String onSale, double listPrice) {
+        super(isbn);
+        setOnSale(onSale);
+        setDiscount(discount);
+        setPrice(listPrice);
+    }
+    public BookstoreBook (String title, String isbn, double discount, String onSale, double listPrice) {
+        super(title, isbn);
+        setOnSale(onSale);
+        setDiscount(discount);
+        setPrice(listPrice);
+    }
+    public BookstoreBook (String author, String title, String isbn, double discount, String onSale, double listPrice) {
+        super(author, title, isbn);
+        setOnSale(onSale);
+        setDiscount(discount);
+        setPrice(listPrice);
+    }
+
+    @Override
+    public String toString () {
+        return "[" + getIsbn() + "-" + getTitle() + " by " + getAuthor() + ", $" + listPrice +
+                " listed for $" + String.format("%.2f", getPriceReduction()) + "]";
+    }
 }
 //_________________________________
 class LibraryBook extends Book {
-    //fields and specific code to the LibraryBook class goes here
+    //random number variable
+    Random randInt = new Random();
+    
+    //callNum itself
+    private String callNum;
+
+    //fields for callNum constituents
+    private int floorNumber;
+    private String firstThreeChar;
+    private char callIsbn;
+
+    //setters for constituents
+    public void setFloorNumber() {
+        this.floorNumber = randInt.nextInt(1, 100); //set random number each object
+    }
+    public void setFirstThreeChar() {
+        this.firstThreeChar = getAuthor().substring(0,3); //grabs the first three characters of String author
+    }
+    public void setCallIsbn() {
+        this.callIsbn = getIsbn().charAt(getIsbn().length() - 1); //takes the char value at the last index of Isbn
+    }
+
+    //piece together the fields into one
+    public void setCallNum() {
+        if(getFloorNumber() < 10)
+            this.callNum = "0%d.%s.%s".formatted(getFloorNumber(), getFirstThreeChar(), getCallIsbn());
+        else
+            this.callNum = "%d.%s.%s".formatted(getFloorNumber(), getFirstThreeChar(), getCallIsbn());
+    }
+    
+    //getters for constituents
+    public int getFloorNumber() {
+        return floorNumber;
+    }
+    public String getFirstThreeChar() {
+        return firstThreeChar;
+    }
+    public char getCallIsbn() {
+        return callIsbn;
+    }
+    
+    //getter for the callNum
+    public String getCallNum() {
+        return callNum;
+    }
+
+    //Constructors
+    public LibraryBook(String isbnStr) {
+        super(isbnStr);
+        setFloorNumber();
+        firstThreeChar = "null";
+        setCallIsbn();
+        setCallNum();
+    }
+    public LibraryBook(String title, String isbnStr) {
+        super(title, isbnStr);
+        setFloorNumber();
+        firstThreeChar = "null";
+        setCallIsbn();
+        setCallNum();
+    }
+    public LibraryBook(String author, String title, String isbnStr) {
+        super(author, title, isbnStr);
+        setFloorNumber();
+        setFirstThreeChar();
+        setCallIsbn();
+        setCallNum();
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getIsbn() + "-" + getTitle() + " by " + getAuthor() + "-" + getCallNum() + "]";
+    }
+
 }
 //_________________________________
 class BookList {
@@ -189,13 +367,26 @@ class BookList {
                 break;
             }
         }
-        if(full) System.out.println("Sorry, no more books can be added to your list.");
+        if(full) {
+            System.out.println("Sorry, no more books can be added to your list.");
+        }
+    }
+    
+    //returns the Book object at the index of list that matches the parameter
+    public Book getBook(String isbnStr) {
+        for(int i = 0; i < 100; i++) {
+            if (list[i] == null) return null;
+            if (isbnStr.compareToIgnoreCase(list[i].getIsbn()) == 0)
+                return list[i];
+        }
+
+        return null;
     }
 
     //prints out the full list of books across all types
     public void printList() {
         int lb = 0, bb = 0;
-        System.out.println("Here are all your books...");
+        System.out.println("\nHere are all your books...");
 
         for(int i = 0; i < 100; i++) {
             if(list[i] instanceof LibraryBook) lb++;
@@ -209,7 +400,7 @@ class BookList {
                 System.out.println("\t\t" + b.toString());
         }
 
-        System.out.println("\n\n----");
+        System.out.println("\n----");
 
         System.out.println("Bookstore Books (" + bb + ")");
         for(Book b: list) {
@@ -217,7 +408,7 @@ class BookList {
                 System.out.println("\t\t" + b.toString());
         }
 
-        System.out.println("\n\n----");
+        System.out.println("\n----");
         System.out.println("Take care now!");
     }
 }
