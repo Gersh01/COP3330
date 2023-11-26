4ed2d5ddb1afd3bfca5a236eff18c6000837faf6
Alex Gershfeld <134902494+Gersh01@users.noreply.github.com>
2023-11-26 18:58:23 -0500

    Update FinalProject.java

--- COP3330/FinalProject.java
+++ COP3330/FinalProject.java
@@ -4,14 +4,17 @@
 //COP3330
 //December 1, 2023
 
-import java.lang.reflect.Array;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class FinalProject {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         int option;
-        String tmpId;
+        String tmpId = null;
         Scanner input = new Scanner(System.in);
 
         //set the Array list to act as our data structure
@@ -24,22 +27,21 @@ public class FinalProject {
             printMenu();
 
             //catch the input for the menu selection, handle any exception with input
-            while (true) {
-                try {
-                    option = input.nextInt();
-                    input.nextLine();
-                    break;
-                } catch (Exception e) {
-                    System.out.println("You entered an invalid input - try again");
-                    continue;
-                }
+            try {
+                option = input.nextInt();
+                input.nextLine();
+
+            } catch (Exception e) {
+                System.out.println("You entered an invalid input - try again");
+                input.nextLine();
+                continue;
             }
 
             //switch to sort selection input to its functionality
             switch (option) {
                 case 1:
                     System.out.println("\nEnter the faculty info:");
-                    System.out.println("----------------------------------------------------");
+                    System.out.println("---------------------------------------------------------------------------");
                     pList.add ( new Faculty
                         (
                             inputName(input),
@@ -52,7 +54,7 @@ public class FinalProject {
                     break;
                 case 2:
                     System.out.println("\nEnter the student info:");
-                    System.out.println("----------------------------------------------------");
+                    System.out.println("---------------------------------------------------------------------------");
                     pList.add ( new Student
                         (
                             inputName(input),
@@ -64,14 +66,18 @@ public class FinalProject {
                     System.out.println("\nStudent member added!");
                     break;
                 case 3:
-                    System.out.println("3 - Print tuition invoice for a student");
+                    System.out.print("Enter the Student's id: ");
+                    if(!printObject(input, pList))
+                        System.out.println("\nNo Student matched!");
                     break;
                 case 4:
-                    System.out.println("4 - Print faculty information");
+                    System.out.print("Enter the Faculty's id: ");
+                    if(!printObject(input, pList))
+                        System.out.println("\nNo Faculty member matched!");
                     break;
                 case 5:
                     System.out.println("\nEnter the staff info: ");
-                    System.out.println("----------------------------------------------------");
+                    System.out.println("---------------------------------------------------------------------------");
                     pList.add ( new Staff
                         (
                             inputName(input),
@@ -83,17 +89,28 @@ public class FinalProject {
                     System.out.println("\nStaff member added!");
                     break;
                 case 6:
-                    System.out.println("Enter the Staff's id: ");
+                    System.out.print("Enter the Staff's id: ");
                     if(!printObject(input, pList))
-                        System.out.println("No Staff member matched!");
-
+                        System.out.println("\nNo Staff member matched!");
                     break;
                 case 7:
-                    System.out.println("7 - Delete a person");
+                    boolean exist = false;
+                    System.out.print("Enter the id of the person to delete: ");
+                    tmpId = input.nextLine();
+                    for(Person p: pList) {
+                        if(p.getId().compareToIgnoreCase(tmpId) == 0) {
+                            System.out.println("\n" + p.getName() + " successfully deleted");
+                            pList.remove(p);
+                            exist = true;
+                            break;
+                        }
+                    }
+                    if(!exist)
+                        System.out.println("\nSorry no such person exists");
                     break;
                 case 8:
                     sentinel = false;
-                    exitProtocol(input);
+                    exitProtocol(input, pList);
                     break;
                 default:
                     System.out.println("You entered an invalid input - try again");
@@ -103,7 +120,7 @@ public class FinalProject {
     }
     //method to be called every cycle of instruction
     public static void printMenu() {
-        System.out.println("----------------------------------------------------");
+        System.out.println("---------------------------------------------------------------------------");
         System.out.println("Choose one of the options:");
         System.out.println("1 - Enter the information of a faculty");
         System.out.println("2 - Enter the information of a student");
@@ -113,7 +130,7 @@ public class FinalProject {
         System.out.println("6 - Print the information of a staff member");
         System.out.println("7 - Delete a person");
         System.out.println("8 - Exit Program");
-        System.out.println("----------------------------------------------------");
+        System.out.println("---------------------------------------------------------------------------");
         System.out.print("Enter your selection: ");
     }
 
@@ -138,7 +155,7 @@ public class FinalProject {
         String fullName = null;
         while (true) {
             try {
-                System.out.print("Name of the faculty: ");
+                System.out.print("Name of the person: ");
                 fullName = input.nextLine();
 
                 for (int i = 0; i < fullName.length(); i++)
@@ -241,7 +258,7 @@ public class FinalProject {
         double gpa = 0;
         while(true) {
             try {
-                System.out.println("Enter GPA between 0.00 - 4.00");
+                System.out.print("Enter GPA between 0.00 - 4.00: ");
                 gpa = input.nextDouble();
                 input.nextLine();
 
@@ -259,7 +276,7 @@ public class FinalProject {
         int creditHours = 0;
         while(true) {
             try {
-                System.out.println("Enter credit hours taken - must be greater than 0: ");
+                System.out.print("Enter credit hours taken - must be greater than 0: ");
                 creditHours = input.nextInt();
                 input.nextLine();
 
@@ -272,7 +289,7 @@ public class FinalProject {
         }
         return creditHours;
     }
-    
+
     //matching ids and printing the object
     public static boolean printObject(Scanner input, ArrayList<Person> list) {
         String tmpId = null;
@@ -287,7 +304,7 @@ public class FinalProject {
     }
 
     //saves a file with sorted list based on user input -- exits sequence
-    public static void exitProtocol(Scanner input) {
+    public static void exitProtocol(Scanner input, ArrayList<Person> list) throws FileNotFoundException {
         String reportOption = null;
         int sortOption = 0;
         while(true) {
@@ -316,18 +333,82 @@ public class FinalProject {
                     continue;
                 }
             }
-            if(sortOption == 1) { //sort gpa in descending order
 
-            }
-            else { //sort names in descending order
-
-            }
+            reportWriter(sortOption, list);
             System.out.println("Report created and saved on your hard drive!");
         }
         System.out.println("Goodbye!");
     }
+    public static void reportWriter(int option, ArrayList<Person> list) throws FileNotFoundException {
+        PrintWriter file = new PrintWriter("report.txt");
+        int listedObject = 1;
+
+        file.println("Report created on " + java.time.LocalDate.now());
+        file.println("****************************************");
+        file.println("\nFaculty Members");
+        file.println("----------------------------------------");
+
+        //Printing the faculty
+        for (Person value : list) {
+            if (value instanceof Faculty) {
+                file.println("\t" + listedObject++ + ". " + value.getName());
+                file.println("\tID: " + value.getId());
+                file.println("\t" + ((Faculty) value).getRank() + ", " + ((Faculty) value).getDepartment() + "\n");
+            }
+        }
+
+        file.println("\nStaff Members");
+        file.println("----------------------------------------");
+
+        //Printing the staff
+        listedObject = 1;
+        for (Person person : list) {
+            if (person instanceof Staff) {
+                file.println("\t" + listedObject++ + ". " + person.getName());
+                file.println("\tID: " + person.getId());
+                file.println("\t" + ((Staff) person).getDepartment() + ", " + ((Staff) person).getStatus() + "\n");
+            }
+        }
+
+        //Printing the students
+        ArrayList<Student> sortList = new ArrayList<Student>();
+        listedObject = 1;
+        for(Person p: list)
+            if(p instanceof Student)
+                sortList.add((Student)p);
+        if(option == 1) {
+            file.println("\nStudents Members (sorted by descending GPA)");
+            file.println("----------------------------------------");
+
+            sortList.sort(Comparator.comparingDouble(Student::getGpa));
+            for (Student s: sortList) {
+                file.println("\t" + listedObject++ + ". " + s.getName());
+                file.println("\tID: " + s.getId());
+                file.println("\tGpa: " + s.getGpa());
+                file.println("\tCredit hours: " + s.getCreditHours() + "\n");
+            }
+        }
+        else {
+            file.println("\nStudents Members (sorted by descending name)");
+            file.println("----------------------------------------");
+
+            sortList.sort(Comparator.comparing(Student::getName).reversed());
+            for (Student s: sortList) {
+                file.println("\t" + listedObject++ + ". " + s.getName());
+                file.println("\tID: " + s.getId());
+                file.println("\tGpa: " + s.getGpa());
+                file.println("\tCredit hours: " + s.getCreditHours() + "\n");
+            }
+        }
+
+        file.flush();
+        file.close();
+    }
 }
 
+
+
+//---------------------------------
 interface Personable {
     void print();
 }
@@ -370,7 +451,7 @@ abstract class Person implements Personable {
 class Student extends Person {
     private double gpa;
     private int creditHours;
-    
+
     public double getGpa() {
         return gpa;
     }
@@ -398,17 +479,20 @@ class Student extends Person {
 
     @Override
     public void print() {
+        double total = (getCreditHours() * 236.45) + 52;
+        double discount = (getCreditHours() * 236.45 * .25);
+        double totalDiscount = total - discount;
+        
         System.out.println("---------------------------------------------------------------------------");
         System.out.println(getName() + "\t" + getId());
-        System.out.println("Credit hours:" + getCreditHours() + " ($236.45/credit hour)");
+        System.out.println("Credit hours: " + getCreditHours() + " ($236.45/credit hour)");
         System.out.println("Fees: $52");
         if(getGpa() >= 3.85) {
-            System.out.println("Total payment (after discount): $" + ((getCreditHours() * 236.45) - (getCreditHours() * 236.45 * .25)) + "\t" + "(" + (getCreditHours() * 236.45 * .25) + "$ discount applied)");
+            System.out.println("Total payment (after discount): $" + String.format("%.2f", totalDiscount) + "\t" + "(" + String.format("%.2f", discount) + "$ discount applied)");
         }
         else {
-            System.out.println("Total payment (after discount): $" + (getCreditHours() * 236.45) + "\t" + "(0$ discount applied)");
+            System.out.println("Total payment (after discount): $" + String.format("%.2f", total) + "\t" + "(0$ discount applied)");
         }
-        System.out.println("---------------------------------------------------------------------------");
     }
 }
 
@@ -417,7 +501,13 @@ abstract class Employee extends Person implements Personable {
     private String department;
 
     public String getDepartment() {
-        return department;
+        String dep = department;
+
+        if(department.compareToIgnoreCase("mathematics") == 0) dep = "Mathematics";
+        else if(department.compareToIgnoreCase("english") == 0) dep = "English";
+        else dep = "Engineering";
+
+        return dep;
     }
 
     public void setDepartment(String department) {
@@ -442,7 +532,12 @@ class Faculty extends Employee {
     private String rank;
 
     public String getRank() {
-        return rank;
+        String tmpRank = rank;
+
+        if(rank.compareToIgnoreCase("professor") == 0) tmpRank = "Professor";
+        else tmpRank = "Adjunct";
+
+        return tmpRank;
     }
 
     public void setRank(String rank) {
@@ -462,9 +557,8 @@ class Faculty extends Employee {
     @Override
     public void print() {
         System.out.println("---------------------------------------------------------------------------");
-        System.out.println(getName() + "\t" + getId());
-        System.out.println(getDepartment() + ", " + getRank());
-        System.out.println("---------------------------------------------------------------------------");
+        System.out.println(getName() + "\t\t" + getId());
+        System.out.println(getDepartment() + " Department, " + getRank());
     }
 }
 
@@ -473,7 +567,12 @@ class Staff extends Employee {
     private String status;
 
     public String getStatus() {
-        return status;
+        String tmpStatus = status;
+
+        if(tmpStatus.compareToIgnoreCase("p") == 0) tmpStatus = "Part Time";
+        else tmpStatus = "Full Time";
+
+        return tmpStatus;
     }
 
     public void setStatus(String status) {
@@ -493,14 +592,8 @@ class Staff extends Employee {
     @Override
     public void print() {
         System.out.println("---------------------------------------------------------------------------");
-        System.out.println(getName() + "\t" + getId());
-        if(getStatus().compareToIgnoreCase("f") == 0) {
-        	System.out.println(getDepartment() + ", Full Time");
-        }
-        
-        else if(getStatus().compareToIgnoreCase("p") == 0) {
-        	System.out.println(getDepartment() + ", Part Time");
-        }
-        System.out.println("---------------------------------------------------------------------------");
+        System.out.println(getName() + "\t\t" + getId());
+        System.out.println(getDepartment() + " Department, " + getStatus());
+
     }
 }
