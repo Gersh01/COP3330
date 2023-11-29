e9aa7e7d7ca382ae4a03e889c9febd1aca773800
GabrielFPerez <115119707+GabrielFPerez@users.noreply.github.com>
2023-11-29 13:49:08 -0500

    Update project.c

--- project.c
+++ project.c
@@ -4,62 +4,51 @@
 /* 10 Points */
 void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
 {
-
+    
 }
 
 /* instruction fetch */
 /* 10 Points */
 int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
-{
-    *instruction = Mem[PC >> 2];
-    PC += 4;
+{   
+    //(sizeof(Mem) < (65536 >> 2)
+    if(PC % 4 == 0) {
+        *instruction = Mem[PC >> 2];
+        return 0;
+    }
+    else {
+        return 1;
+    }
     
-    return 0;
 }
 
 /* instruction partition */
 /* 10 Points */
 void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsigned *r2, unsigned *r3, unsigned *funct, unsigned *offset, unsigned *jsec)
 {
-    *op = (instruction & 0xFC000000) >> 26;
-    
-    if(!(*op ^ 0)) { //r-type
-        *r1 = (instruction & 0x03E00000);
-        *r2 = (instruction & 0x00180000);
-        *r3 = (instruction & 0x00000F80);
-        *offset = (instruction & 0x000007B0);
-        *funct = (instruction & 0x0000003F); 
-    }
-    else if(*op < 8) { //j-type
-        *jsec = (instruction & 0x03FFFFFF);
-    }
-    else { //i-type
-        *r1 = (instruction & 0x03E00000);
-        *r2 = (instruction & 0x00180000);
-        *offset = (instruction & 0x0000FFFF);
-    }
-    
+
 }
 
 /* instruction decode */
 /* 15 Points */
 int instruction_decode(unsigned op,struct_controls *controls)
 {
-
+    
 }
 
 /* Read Register */
 /* 5 Points */
 void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigned *data2)
 {
-
+    data1 = Reg[r1];
+    data2 = Reg[r2];
 }
 
 /* Sign Extend */
 /* 10 Points */
 void sign_extend(unsigned offset,unsigned *extended_value)
 {
-
+    extended_value = offset << 16;
 }
 
 /* ALU operations */
