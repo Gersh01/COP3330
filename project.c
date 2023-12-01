86273f49631e0c16a2293f3b794ddaf1ec463485
GabrielFPerez <115119707+GabrielFPerez@users.noreply.github.com>
2023-12-01 14:35:32 -0500

    Update project.c

--- project.c
+++ project.c
@@ -1,5 +1,5 @@
 #include "spimcore.h"
-
+#define MEMSIZE (65536 >> 2)
 /* ALU */
 /* 10 Points */
 void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
@@ -11,8 +11,7 @@ void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
 /* 10 Points */
 int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
 {   
-    //(sizeof(Mem) < (65536 >> 2)
-    if(PC % 4 == 0) {
+    if(PC % 4 == 0 && Mem[PC >> 2] < MEMSIZE) {
         *instruction = Mem[PC >> 2];
         return 0;
     }
@@ -24,7 +23,7 @@ int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
 
 /* instruction partition */
 /* 10 Points */
-void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsigned *r2, unsigned *r3, unsigned *funct, unsigned *offset, unsigned *jsec)
+void instruction_partition(unsigned instruction, unsigned* op, unsigned* r1, unsigned* r2, unsigned* r3, unsigned* funct, unsigned* offset, unsigned* jsec)
 {
    *op = (instruction & 0xFC000000) >> 26;
     
@@ -43,28 +42,139 @@ void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsi
         *r2 = (instruction & 0x00180000) >> 16;
         *offset = (instruction & 0x0000FFFF);
     }
-    
 }
 
 /* instruction decode */
 /* 15 Points */
-int instruction_decode(unsigned op,struct_controls *controls)
+int instruction_decode(unsigned op, struct_controls* controls)
 {
-
+	switch(op) {
+        case 0: //r-type: add, subtract, and, xor, slt, and sltu
+          	controls->RegDst = 1;
+        		controls->Jump = 0;
+        		controls->Branch = 0;
+        		controls->MemRead = 0;
+        		controls->MemtoReg = 0;
+        		controls->ALUOp = 7; //depends on funct
+        		controls->MemWrite = 0;
+        		controls->ALUSrc = 0;
+        		controls->RegWrite = 1;
+         	  break;
+        case 2: //j-type
+          	controls->RegDst = 0;
+        		controls->Jump = 1;
+        		controls->Branch = 0;
+        		controls->MemRead = 0;
+        		controls->MemtoReg = 0;
+        		controls->ALUOp = 0;
+        		controls->MemWrite = 0;
+        		controls->ALUSrc = 0;
+        		controls->RegWrite = 0;
+  	    	  break;
+        case 4: //beq
+          	controls->RegDst = 2;
+        		controls->Jump = 0;
+        		controls->Branch = 1;
+        		controls->MemRead = 0;
+        		controls->MemtoReg = 2;
+        		controls->ALUOp = 1; //subtract
+        		controls->MemWrite = 0;
+        		controls->ALUSrc = 0;
+        		controls->RegWrite = 0;  
+       	    break;
+        case 8: //addi
+          	controls->RegDst = 0;
+        		controls->Jump = 0;
+        		controls->Branch = 0;
+        		controls->MemRead = 0;
+        		controls->MemtoReg = 0;
+        		controls->ALUOp = 0; //add
+        		controls->MemWrite = 0;
+        		controls->ALUSrc = 1;
+        		controls->RegWrite = 1;
+          	break;
+        case 10: //slti
+          	controls->RegDst = 0;
+        		controls->Jump = 0;
+        		controls->Branch = 0;
+        		controls->MemRead = 0;
+        		controls->MemtoReg = 0;
+        		controls->ALUOp = 2;
+        		controls->MemWrite = 0;
+        		controls->ALUSrc = 1;
+        		controls->RegWrite = 0;
+      		  break;
+        case 11: //sltiu
+          	controls->RegDst = 0;
+        		controls->Jump = 0;
+        		controls->Branch = 0;
+        		controls->MemRead = 0;
+        		controls->MemtoReg = 0;
+        		controls->ALUOp = 3;
+        		controls->MemWrite = 0;
+        		controls->ALUSrc = 1;
+        		controls->RegWrite = 0;
+          	break;
+        case 15: //lui
+          	controls->RegDst = 0;
+        		controls->Jump = 0;
+        		controls->Branch = 0;
+        		controls->MemRead = 0;
+        		controls->MemtoReg = 1;
+        		controls->ALUOp = 6;
+        		controls->MemWrite = 0;
+        		controls->ALUSrc = 1;
+        		controls->RegWrite = 0;
+          	break;
+        case 35: //lw
+          	controls->RegDst = 0;
+        		controls->Jump = 0;
+        		controls->Branch = 0;
+        		controls->MemRead = 1;
+        		controls->MemtoReg = 1;
+        		controls->ALUOp = 0; //add
+        		controls->MemWrite = 0;
+        		controls->ALUSrc = 1;
+        		controls->RegWrite = 1;
+          	break;
+        case 43: //sw
+          	controls->RegDst = 0;
+        		controls->Jump = 0;
+        		controls->Branch = 0;
+        		controls->MemRead = 0;
+        		controls->MemtoReg = 0;
+        		controls->ALUOp = 0; //add
+        		controls->MemWrite = 1;
+        		controls->ALUSrc = 1;
+        		controls->RegWrite = 0;
+          	break;
+        default:
+          return 1;
+    }
+    return 0;
 }
 
 /* Read Register */
 /* 5 Points */
 void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigned *data2)
 {
-
+    *data1 = *Reg[r1];
+    *data2 = *Reg[r2]; 
 }
 
 /* Sign Extend */
 /* 10 Points */
 void sign_extend(unsigned offset,unsigned *extended_value)
 {
-    extended_value = offset << 16;
+    unsigned signbit = offset >> 15;
+    if (signbit == 0) {
+        *extended_value = 0b00000000000000000000000000000000 + offset;
+    }
+    
+    else {
+        *extended_value = 0b11111111111111110000000000000000 + offset;
+    }
+    
 }
 
 /* ALU operations */
@@ -85,12 +195,34 @@ int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsig
 /* 10 Points */
 void write_register(unsigned r2,unsigned r3,unsigned memdata,unsigned ALUresult,char RegWrite,char RegDst,char MemtoReg,unsigned *Reg)
 {
-
+    if(RegWrite == 1 && MemtoReg == 1) { //bring data from memory
+        *Reg[r3] = memdata;
+    }
+    else if (RegWrite == 1 && MemtoReg == 0) { //bring data from aluresult
+        *Reg[r2] = ALUresult;
+    }
 }
 
 /* PC update */
 /* 10 Points */
 void PC_update(unsigned jsec,unsigned extended_value,char Branch,char Jump,char Zero,unsigned *PC)
 {
-
+    if (Jump == 0 && Branch == 0) {
+        *PC = *PC + 4;
+    }
+    else if (Jump == 1) {
+        *PC = (*PC >> 27) + (jsec << 2);
+      
+    }
+    else if (Branch == 1 && Zero == 1) {
+        if (extended_value >> 31 == 0) {
+            *PC = *PC + (extended_value - 0b00000000000000000000000000000000)  * 4;  
+        }
+        else if (extended_value >> 31 == 1) {
+            *PC = *PC + (extended_value - 0b11111111111111110000000000000000)  * 4;
+        }
+         
+    }
+    
+    
 }
